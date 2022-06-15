001b1d23d5f46738d64f57def64bbed59d0d400b
unknown <ryu.faryan@binus.ac.id>
2022-06-15 14:31:40 +0700

    add product detail

--- app/src/main/java/com/binus/idea/ProductDetail.java
+++ app/src/main/java/com/binus/idea/ProductDetail.java
@@ -1,8 +1,31 @@
 package com.binus.idea;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class ProductDetail extends AppCompatActivity {
+    TextView price;
+    ImageView photos;
+    TextView title;
+    Button buy;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_detail);
+        title = findViewById();
+        photos = findViewById();
+        price = findViewById();
+        //(.....).setOnClickListener(v -> {
+        //  Product product = com.binus.idea.Product.getInstance();
+        //  product.setData(product_name);
+        //  product.setData(product_image);
+        //  product.setData(product_price);
+        //  (.....).setText(product.getData());
+        //}
+
+    }
 }
